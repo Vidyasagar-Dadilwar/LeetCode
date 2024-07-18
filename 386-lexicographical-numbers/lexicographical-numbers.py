8c4bab81dfbfb0079f41063b2c163ec55b9d1c5e
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2024-07-18 15:57:52 +0530

    Time: 83 ms (39.61%) | Memory: 22.1 MB (97.81%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,10 +1,17 @@
 class Solution:
+    def solve(self, n, current, l):
+        if current > n:
+            return
+        l.append(current)
+        for i in range(10):
+            if current * 10 + i > n:
+                return
+            self.solve(n, current * 10 + i, l)
+
     def lexicalOrder(self, n: int) -> List[int]:
-        l=[]
-        for i in range(1, n+1):
-            l.append(str(i))
-        l.sort()
-        ans=[]
-        for i in l:
-            ans.append(int(i))
-        return ans
+        l = []
+        for i in range(1, 10):
+            if i > n:
+                break
+            self.solve(n, i, l)
+        return l
