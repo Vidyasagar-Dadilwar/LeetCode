234f40b3e702716206578ab536d0e89258f78fa6
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2024-07-24 18:43:07 +0530

    Time: 31 ms (28.46%) | Memory: 11 MB (24.17%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    string minWindow(std::string s, std::string t) {
-        if (t.size() > s.size()) {
-            return "";
-        }
-        unordered_map<char, int> mp;
-        for (char c : t) {
+    string minWindow(string s, string t) {
+        unordered_map<char, int>mp;
+        for(auto c: t){
             mp[c]++;
         }
-        int l = 0, r = 0, minLen = INT_MAX, start = 0;
-        int required = t.size();        
-        while (r < s.size()) {
-            if (mp[s[r]] > 0) {
-                required--;
+        int l=0, r=0, minLen=INT_MAX, start=0;
+        int req=mp.size(), form=0;
+        unordered_map<char, int>mppp;
+        while(r<s.size()){
+            mppp[s[r]]++;
+            if(mp.count(s[r]) && mppp[s[r]]==mp[s[r]]){
+                form++;
             }
-            mp[s[r]]--;
-            r++;
-            while (required == 0) {
-                if (r - l < minLen) {
-                    minLen = r - l;
-                    start = l;
+            while(l<=r && form==req){
+                if(r-l+1<minLen){
+                    minLen=r-l+1;
+                    start=l;
                 }
-                mp[s[l]]++;
-                if (mp[s[l]] > 0) {
-                    required++;
+                mppp[s[l]]--;
+                if(mp.count(s[l]) && mppp[s[l]]<mp[s[l]]){
+                    form--;
                 }
                 l++;
             }
+            r++;
         }
-        return (minLen == INT_MAX) ? "" : s.substr(start, minLen);
+        return (minLen==INT_MAX)?"" : s.substr(start, minLen);
     }
 };
