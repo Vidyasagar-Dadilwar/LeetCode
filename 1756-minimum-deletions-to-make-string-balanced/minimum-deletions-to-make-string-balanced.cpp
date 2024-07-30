efefc734d9b4a939cb0697940f812ca477703fa2
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2024-07-30 17:23:08 +0530

    Time: 80 ms (81.18%) | Memory: 23.7 MB (73.37%) - LeetSync

--- 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
+++ 1756-minimum-deletions-to-make-string-balanced/minimum-deletions-to-make-string-balanced.cpp
@@ -3,14 +3,14 @@ public:
     int minimumDeletions(string s) {
         int cnt=0, bcnt=0;
         for(auto i: s){
-            if(i == 'b') 
+            if(i == 'b'){
                 bcnt++;
-            else if(bcnt>0){
-                cnt++;
+            }
+            else if(bcnt > 0){
                 bcnt--;
+                cnt++;
             }
         }
         return cnt;
-
     }
 };
