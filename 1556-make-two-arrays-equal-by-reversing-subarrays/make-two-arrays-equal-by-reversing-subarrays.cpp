87bad07cdc23131564fa1c87d944fd51618f9945
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2024-08-03 08:32:01 +0530

    Time: 3 ms (98.33%) | Memory: 17.6 MB (42.71%) - LeetSync

--- 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/make-two-arrays-equal-by-reversing-subarrays.cpp
@@ -2,17 +2,8 @@
 class Solution {
 public:
     bool canBeEqual(vector<int>& target, vector<int>& arr) {
-        map<int, int>mp1;
-        map<int, int>mp2;
-
-        for (int num : target) {
-            mp1[num]++;
-        }
-
-        for (int num : arr) {
-            mp2[num]++;
-        }
-
-        return mp1 == mp2;
+        sort(target.begin(), target.end());
+        sort(arr.begin(), arr.end());
+        return target == arr;
     }
 };
