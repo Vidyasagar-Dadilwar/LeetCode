e50edca8aa65b2f3b98fc1108a09adf867fa4684
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2024-08-14 08:57:03 +0530

    Time: 17 ms (35.65%) | Memory: 12.7 MB (71.29%) - LeetSync

--- 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
+++ 719-find-k-th-smallest-pair-distance/find-k-th-smallest-pair-distance.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int smallestDistancePair(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
-        int min_dist = 0, max_dist = nums.back() - nums.front();
-        int l = min_dist, h = max_dist;
+        int min_dist=0, max_dist=nums.back()-nums.front();
+        int l=min_dist, h=max_dist;
         while(l!=h){
-            auto e = nums.end();
-            auto mid = l+(h-l)/2;
+            auto e=nums.end();
+            int mid = l+(h-l)/2;
             int x = -nums.size();
-            for(auto i = nums.begin(); x<k && i!=e; i++){
-                // last pair jaha tk dist less than mid rhega uska aur 
-                // current pos ka difference defines possible pairs 
+            for(auto i=nums.begin(); i<e && x<k; i++){
                 x += upper_bound(i, e, (*i)+mid) - i;
             }
             if(x<k){
-                l = mid+1;
+                l=mid+1;
             }
             else{
-                h = mid;
+                h=mid;
             }
         }
         return l;
