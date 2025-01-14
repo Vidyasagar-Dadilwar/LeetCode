5ebf6d768df36b989f5c0e75cc6d953cdb5a9325
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-01-14 11:20:19 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.75%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,21 +1,22 @@
-#include<math.h>
 class Solution {
 public:
-    double solve(double x, int n){
-        if(n==0) 
-            return 1;
-        if(n==1) 
-            return x;
-        if(n<0){
-            n=abs(n);
-            x=1/x;
-        }    
-        if(n%2==0) 
-            return myPow(x*x,n/2);
-        else 
-            return x*myPow(x,n-1);
-    }
     double myPow(double x, int n) {
-        return solve(x, n);
+        long pow = n;
+        double ans = 1;
+        
+        
+        if(pow < 0){
+            x = 1/x; 
+            pow = -pow; 
+        }
+
+        while(pow > 0){
+            if(pow % 2 == 1){
+                ans *= x;
+            }
+            x *= x;
+            pow /= 2;
+        }
+        return ans;
     }
 };
