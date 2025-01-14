2073b35e378c62cc4d889ec6c5c9a5f527c26ba6
Vidyasagar-Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-01-14 11:06:46 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (5.32%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -1,34 +1,28 @@
 class Solution {
 public:
-    int solve(string& s, int i, int sign, int result){
-        if(i==s.size() || !isdigit(s[i])){
-            return result*sign;
-        }
-        int digit=s[i]-'0';
-        if (result > (INT_MAX - digit) / 10) {
+    int solve(string s, int i, int sign, int res){
+        if(i == s.size() || !isdigit(s[i]))
+            return res*sign;
+        int digit = s[i]-'0';
+        if(res > (INT_MAX-digit)/10)
             return (sign == 1) ? INT_MAX : INT_MIN;
-        }
-        result=result*10 + digit;
+        res = res*10 + digit;
         i++;
-        return solve(s, i, sign, result);
+        return solve(s, i, sign, res);
     }
-    int helper(string &s, int i, int sign){
-        while(i<s.size() && s[i]==' '){
+    int helper(string s, int i, int sign){
+        while(s[i] == ' ')
             i++;
-        }
-        // base condition
-        if(i==s.size()){
+        if(i == s.size())
             return 0;
-        }
-        // sign determination
-        if(s[i]=='-' || s[i]=='+'){
-            sign=(s[i]=='-')?-1:1;
+        if(s[i] == '-' || s[i] == '+'){
+            sign = (s[i] == '-') ? -1 : 1;
             i++;
         }
         return solve(s, i, sign, 0);
     }
     int myAtoi(string s) {
-        int ind=0, sign=1;
-        return helper(s, ind, sign);
+        int i=0, sign=1;
+        return helper(s, i, sign);
     }
 };
