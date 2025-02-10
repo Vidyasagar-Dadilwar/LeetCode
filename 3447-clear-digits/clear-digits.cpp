363bee475d59fcfde9268d4dead2033864c364e0
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-02-10 10:36:08 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (56.73%) - LeetSync

--- 3447-clear-digits/clear-digits.cpp
+++ 3447-clear-digits/clear-digits.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        int n = s.size();
-        vector<int> mark(n, 0);
-        for(int i=0; i<n; i++){
-            if(s[i] >= '0' && s[i] <= '9'){
-                mark[i] = 1;
-                for(int j = i; j>=0; j--){
-                    if(mark[j] == 0){
-                        mark[j] = 1;
-                        break;
-                    }
+        string ans = "";        
+        for (char c : s) {
+            if (isdigit(c)) {
+                if (!ans.empty()) {
+                    ans.pop_back(); 
                 }
+            } 
+            else {
+                ans.push_back(c);
             }
-        }
-        string ans = "";
-        for(int i=0; i<n; i++){
-            if(mark[i] == 0)
-                ans += s[i];
-        }
+        }        
         return ans;
     }
 };
