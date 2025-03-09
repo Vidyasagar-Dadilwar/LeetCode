f62e5f5dac9ad83e38ba42574bab0247be43da71
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-03-09 11:20:29 +0530

    Time: 73 ms (50.84%) | Memory: 108 MB (38.70%) - LeetSync

--- 3483-alternating-groups-ii/alternating-groups-ii.cpp
+++ 3483-alternating-groups-ii/alternating-groups-ii.cpp
@@ -2,20 +2,21 @@ class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& colors, int k) {
         int n = colors.size();
+        int cnt = 1, ans = 0;
         for(int i=0; i<k-1; i++)
             colors.push_back(colors[i]);
-        
-        int cnt = 1, ans = 0;
+
         for(int i=1; i<colors.size(); i++){
-            if(colors[i] != colors[i-1])
+            if(colors[i] != colors[i-1]){
                 cnt++;
+            }
             else{
                 if(cnt >= k)
                     ans += (cnt-k+1);
                 cnt = 1;
             }
         }
-        if(cnt >= k) 
+        if(cnt >= k)
             ans += (cnt-k+1);
         return ans;
     }
