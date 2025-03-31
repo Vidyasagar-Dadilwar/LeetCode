661b3919b4b88ce9d0a1217b825cf4face024dbe
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-03-31 18:04:29 +0530

    Time: 113 ms (15.19%) | Memory: 73.3 MB (12.66%) - LeetSync

--- 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.cpp
@@ -1,14 +1,15 @@
 class Solution {
 public:
     long long putMarbles(vector<int>& weights, int k) {
-        priority_queue<int> less, grt;
+        priority_queue<int> grt;
+        priority_queue<int, vector<int>, greater<int>> less;
         for(int i=0; i<weights.size()-1; i++){
-            less.push((weights[i] + weights[i+1])*-1);
+            less.push((weights[i] + weights[i+1]));
             grt.push(weights[i] + weights[i+1]);
         }
         long long ans = 0;
         for(int i=1; i<k; i++){
-            ans += less.top();
+            ans -= less.top();
             less.pop();
             ans += grt.top();
             grt.pop();
