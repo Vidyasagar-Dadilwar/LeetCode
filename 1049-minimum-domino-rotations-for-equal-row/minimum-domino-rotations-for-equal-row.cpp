0df1f8f4c44385a3d5e478a47b5fd5714baf535f
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-05-03 23:04:46 +0530

    Time: 0 ms (100.00%) | Memory: 115.4 MB (76.51%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minDominoRotations(vector<int>& tops, vector<int>& bottoms) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
         int res = INT_MAX;
         for (int j = 1; j <= 6; ++j) {
             bool flag = true;
