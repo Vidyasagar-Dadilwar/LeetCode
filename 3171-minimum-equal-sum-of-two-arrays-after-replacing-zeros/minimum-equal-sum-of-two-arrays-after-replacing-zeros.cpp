45867cf58e8c81976a7a066db44fa9067f5605b1
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-05-10 21:38:18 +0530

    Time: 80 ms (95.93%) | Memory: 136.1 MB (5.08%) - LeetSync

--- 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
+++ 3171-minimum-equal-sum-of-two-arrays-after-replacing-zeros/minimum-equal-sum-of-two-arrays-after-replacing-zeros.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     long long minSum(vector<int>& nums1, vector<int>& nums2) {
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        cout.tie(NULL);
         long long s1 = 0, s2 = 0, z1 = 0, z2 = 0;
         for (int num : nums1) {
             if (num == 0) 
