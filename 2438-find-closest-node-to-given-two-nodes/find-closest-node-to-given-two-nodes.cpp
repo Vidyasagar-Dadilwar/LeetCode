8060938f3766b5543d47fa7c8406a4713600d1d7
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-05-30 12:17:15 +0530

    Time: 19 ms (55.33%) | Memory: 109.4 MB (35.00%) - LeetSync

--- 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
+++ 2438-find-closest-node-to-given-two-nodes/find-closest-node-to-given-two-nodes.cpp
@@ -1,36 +1,33 @@
 class Solution {
 public:
-    void getDistances(vector<int>& edges, int start, vector<int>& dist) {
-        int n = edges.size();
-        vector<bool> visited(n, false);
+    void getDistance(vector<int> & edges, int node, vector<int>& dist){
+        vector<int> vis(edges.size(), 0);
         int d = 0;
-        while (start != -1 && !visited[start]) {
-            dist[start] = d++;
-            visited[start] = true;
-            start = edges[start];
+        while(node != -1 && !vis[node]){
+            dist[node] = d++;
+            vis[node] = 1;
+            node = edges[node];
         }
-    }
+    }    
 
     int closestMeetingNode(vector<int>& edges, int node1, int node2) {
         int n = edges.size();
         vector<int> dist1(n, -1), dist2(n, -1);
 
-        getDistances(edges, node1, dist1);
-        getDistances(edges, node2, dist2);
+        getDistance(edges, node1, dist1);
+        getDistance(edges, node2, dist2);
 
-        int minDist = INT_MAX;
-        int result = -1;
+        int minDist = INT_MAX, res = -1;
 
-        for (int i = 0; i < n; ++i) {
-            if (dist1[i] != -1 && dist2[i] != -1) {
-                int maxDist = max(dist1[i], dist2[i]);
-                if (maxDist < minDist) {
+        for(int i=0; i<n; i++){
+            if(dist1[i] != -1 && dist2[i] != -1){
+                int maxDist =  max(dist1[i], dist2[i]);
+                if(maxDist < minDist){
                     minDist = maxDist;
-                    result = i;
+                    res = i;
                 }
             }
         }
-
-        return result;
+        return res;
     }
 }; 
