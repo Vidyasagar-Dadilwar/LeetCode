96a20c1988351e69c46c1238b74e4b0094e78164
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-05-31 20:46:24 +0530

    Time: 3 ms (63.03%) | Memory: 17.6 MB (40.52%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.cpp
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    pair<int, int> intToPos(int sq, int n){
-        int r = (sq - 1) / n, c = (sq - 1) % n;
+    pair<int, int>intToPos(int sq, int n){
+        int r = (sq-1)/n, c = (sq-1)%n;
         if(r%2 != 0)
-            c = n - 1 - c; 
+            c = n-1-c;
         return {r, c};
     }
     int snakesAndLadders(vector<vector<int>>& board) {
-        reverse(board.begin(), board.end());
         int n = board.size();
+        reverse(board.begin(), board.end());
         queue<pair<int, int>> q;
         q.push({1, 0});
         unordered_set<int> vis;
 
         while(!q.empty()){
-            auto it = q.front();
-            int sq = it.first, moves = it.second;
+            auto [sq, moves] = q.front();
             q.pop();
-
+            
             for(int i=1; i<7; i++){
                 int nextSq = sq + i;
-                if(nextSq > n*n)    
+                if(nextSq > n*n)
                     break;
-                auto x = intToPos(nextSq, n);
-                int r = x.first, c = x.second;
+                auto [r, c] = intToPos(nextSq, n);
+
                 if(board[r][c] != -1)
                     nextSq = board[r][c];
                 if(nextSq == n*n)
