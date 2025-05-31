b464c7dace532279e0ac7903f05312cda7762d2e
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-05-31 20:38:51 +0530

    Time: 4 ms (50.45%) | Memory: 17.7 MB (35.04%) - LeetSync

--- 945-snakes-and-ladders/snakes-and-ladders.cpp
+++ 945-snakes-and-ladders/snakes-and-ladders.cpp
@@ -1,38 +1,36 @@
 class Solution {
 public:
     pair<int, int> intToPos(int sq, int n){
-        int r = sq / n, c = sq % n;
-        if(r % 2 != 0)
+        int r = (sq - 1) / n, c = (sq - 1) % n;
+        if(r%2 != 0)
             c = n - 1 - c; 
         return {r, c};
     }
-
     int snakesAndLadders(vector<vector<int>>& board) {
         reverse(board.begin(), board.end());
         int n = board.size();
         queue<pair<int, int>> q;
         q.push({1, 0});
         unordered_set<int> vis;
-        vis.insert(1);
 
         while(!q.empty()){
-            auto [sq, moves] = q.front();
+            auto it = q.front();
+            int sq = it.first, moves = it.second;
             q.pop();
 
-            for(int i = 1; i <= 6; i++){
+            for(int i=1; i<7; i++){
                 int nextSq = sq + i;
-                if(nextSq > n * n) break;
-
-                auto [r, c] = intToPos(nextSq - 1, n);  // 0-based
+                if(nextSq > n*n)    
+                    break;
+                auto x = intToPos(nextSq, n);
+                int r = x.first, c = x.second;
                 if(board[r][c] != -1)
                     nextSq = board[r][c];
-
-                if(nextSq == n * n)
+                if(nextSq == n*n)
                     return moves + 1;
-
                 if(vis.find(nextSq) == vis.end()){
                     vis.insert(nextSq);
-                    q.push({nextSq, moves + 1});
+                    q.push({nextSq, moves+1});
                 }
             }
         }
