0da98a8b96193009e3ddc9ddfe047364ffbfcf83
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-06-07 23:20:30 +0530

    Time: 126 ms (60.39%) | Memory: 26.5 MB (58.12%) - LeetSync

--- 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
+++ 3445-lexicographically-minimum-string-after-removing-stars/lexicographically-minimum-string-after-removing-stars.cpp
@@ -4,7 +4,7 @@ public:
         priority_queue<char, vector<char>, greater<>> heap;
         vector<bool> del(s.size(), false);
         unordered_map<char, stack<int>> mp;
-        int i = 0;
+        int i=0;
         for(auto it: s){
             if(it == '*'){
                 char top = heap.top();
@@ -21,12 +21,11 @@ public:
             }
             i++;
         }
-
-        string res = "";
+        string ans = "";
         for(int i=0; i<s.size(); i++){
             if(s[i] != '*' && del[i] != true)
-                res.push_back(s[i]);
+                ans += s[i];
         }
-        return res;
+        return ans;
     }
 };
