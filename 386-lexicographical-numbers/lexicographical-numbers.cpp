fc83f80ecd59a821e4581eb5d3614a716906d87a
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-06-08 20:43:03 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (69.54%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    void solve(int n, int curr, vector<int>&ans){
-        if(curr>n)
+    void solve(int n, int curr, vector<int>& ans){
+        if(curr > n)
             return;
         ans.push_back(curr);
         for(int i=0; i<10; i++){
-            if(curr*10 + i >n){
+            if(curr*10 + i > n)
                 return;
-            }
-            solve(n, curr*10+i, ans);
+            solve(n, curr*10 + i, ans);
         }
     }
     vector<int> lexicalOrder(int n) {
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        cout.tie(NULL);
         vector<int> ans;
         for(int i=1; i<10; i++){
-            if(i>n)
+            if(i > n)  
                 break;
             solve(n, i, ans);
         }
