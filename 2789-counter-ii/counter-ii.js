8f85213a9401af7f3ba7c336d5a48a7fc1d0a19c
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-08-17 12:31:41 +0530

    Time: 44 ms (78.05%) | Memory: 56.4 MB (69.10%) - LeetSync

--- 2789-counter-ii/counter-ii.js
+++ 2789-counter-ii/counter-ii.js
@@ -2,28 +2,29 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
-var createCounter = function(init) {
-    let currentValue = init; // Initialize currentValue with init
-    
-    return {
-        increment: function() {
-            currentValue += 1;
-            return currentValue;
-        },
-        decrement: function() {
-            currentValue -= 1;
-            return currentValue;
-        },
-        reset: function() {
-            currentValue = init;
-            return currentValue;
-        }
-    };
-};
 
-/** 
+const fs = require('fs')
+var createCounter = function(init) {
+    let n = init;
+    return {
+        increment: function increment(){
+            return ++n;
+        },
+        decrement: function decrement(){
+            return --n;
+        },
+        reset: function reset(){
+            n = init;
+            return init;
+        }
+    }
+};
+process.on('exit', ()=>{
+    // fs.WriteFileSync('display_runtime.txt', '0');
+});
+/**
  * const counter = createCounter(5)
- * console.log(counter.increment()); // 6
- * console.log(counter.reset()); // 5
- * console.log(counter.decrement()); // 4
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
  */
