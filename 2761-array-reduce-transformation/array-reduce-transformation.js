2d676ba9822cac72453c1ca2b0610d0e982a74a7
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-08-17 12:55:06 +0530

    Time: 47 ms (36.90%) | Memory: 55.9 MB (6.77%) - LeetSync

--- 2761-array-reduce-transformation/array-reduce-transformation.js
+++ 2761-array-reduce-transformation/array-reduce-transformation.js
@@ -5,7 +5,7 @@
  * @return {number}
  */
 var reduce = function(nums, fn, init) {
-    for(var i=0; i<nums.length; i++)
-        init = fn(init, nums[i])
-    return init;
+    let res = init;
+    nums.map((it)=> res = fn(res, it));
+    return res;
 };
