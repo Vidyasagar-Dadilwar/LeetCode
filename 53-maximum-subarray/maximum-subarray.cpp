f73ec186edba349d8cabd728e82d3266c47e5e96
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-09-21 15:57:44 +0530

    Time: 0 ms (100.00%) | Memory: 71.8 MB (52.27%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -1,22 +1,15 @@
 class Solution {
 public:
-    bool allNeg(vector<int> arr){
-        for(auto it: arr){
-            if(it >= 0)
-                return false;
-        }
-        return true;
-    }
     int maxSubArray(vector<int>& nums) {
-        int maxi = 0, sum = 0;
+        int maxi = 0, sum = 0, allNeg = 1;
         for(int i=0; i<nums.size(); i++){
             sum += nums[i];
             if(sum < 0)
                 sum = 0;
             maxi = max(maxi, sum);
+            if(nums[i] >= 0)
+                allNeg = 0;
         }
-        if(allNeg(nums))
-            return *max_element(nums.begin(), nums.end());
-        return maxi;
+        return allNeg ? *max_element(nums.begin(), nums.end()) : maxi;
     }
 };
