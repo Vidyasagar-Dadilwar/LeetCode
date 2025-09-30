397d927872c7dcdfc09ccc6fa09ddac610cf3b9a
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-09-30 21:02:37 +0530

    Time: 0 ms (100.00%) | Memory: 19.8 MB (5.16%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,39 +10,24 @@
  */
 class Solution {
 public:
-    ListNode* generateList(vector<int> arr){
-        ListNode* dummyNode=new ListNode(0);
-        ListNode* curr = dummyNode;
-        for(int i=0; i<arr.size(); i++){
-            curr->next=new ListNode(arr[i]);
-            curr=curr->next;
-        }
-        return dummyNode->next;
-    }
-    ListNode* solve(ListNode* l1, ListNode* l2, vector<int>& res){
-        if(l1==NULL && l2==NULL){
-            return generateList(res);
-        }
-        if(l1 == NULL) {
-            res.push_back(l2->val);
-            return solve(l1, l2->next, res);
-        }
-
-        if(l2 == NULL) {
-            res.push_back(l1->val);
-            return solve(l1->next, l2, res);
-        }
-        if(l1->val < l2->val){
-            res.push_back(l1->val);
-            return solve((l1)?l1->next:nullptr, l2, res);
-        }
-        else{
-            res.push_back(l2->val);
-            return solve(l1, (l2)?l2->next:nullptr, res);
-        }
-    }
     ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        vector<int>res;
-        return solve(l1, l2, res);
+        ListNode *dummy = new ListNode(0), *curr = dummy;
+        while(l1!=NULL && l2!=NULL){
+            if(l1->val < l2->val){
+                curr->next = new ListNode(l1->val);
+                curr = curr->next;
+                l1 = l1->next;
+            }
+            else{
+                curr->next = new ListNode(l2->val);
+                curr = curr->next;
+                l2 = l2->next;
+            }
+        }
+        if(l1 != NULL)
+            curr->next = l1;
+        if(l2 != NULL)
+            curr->next = l2;
+        return dummy->next;
     }
 };
