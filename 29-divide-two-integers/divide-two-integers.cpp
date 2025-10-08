66c14a903beae52fb333e103fdae43a09fa2d7bf
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-10-08 22:22:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (96.43%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        if(dividend == numeric_limits<int>::min() && divisor == -1)
-            return numeric_limits<int>::max();
+        if(dividend == INT_MIN && divisor == -1)
+            return INT_MAX;
         return  dividend / divisor;
     }
 };
