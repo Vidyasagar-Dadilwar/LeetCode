8256deb11f2554f8ca5668d776094d59749627b6
Vidyasagar Dadilwar <126343994+Vidyasagar-Dadilwar@users.noreply.github.com>
2025-10-22 11:37:24 +0530

    Time: 387 ms (66.35%) | Memory: 83.4 MB (93.36%) - LeetSync

---
+++ 3640-maximum-frequency-of-an-element-after-performing-operations-ii/maximum-frequency-of-an-element-after-performing-operations-ii.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int solve(vector<int>& nums, int n, int t, int m){
+        long long nL = n, tL = t;
+        int l = lower_bound(nums.begin(), nums.end(), nL) - nums.begin();
+        int h = upper_bound(nums.begin(), nums.end(), nL) - nums.begin();
+        int ll = lower_bound(nums.begin(), nums.end(), nL-tL) - nums.begin();
+        int hh = upper_bound(nums.begin(), nums.end(), nL+tL) - nums.begin();
+        int res = (hh-h) + (l-ll);
+        return min(m, res) + (h-l);
+    }
+    int maxFrequency(vector<int>& nums, int k, int m) {
+        int ans = 1;
+        sort(nums.begin(), nums.end());
+        for(int i=0; i<nums.size()-1; i++){
+            ans = max(solve(nums, nums[i], k, m), ans);
+            ans = max(solve(nums, nums[i]-k, k, m), ans);
+            ans = max(solve(nums, nums[i]+k, k, m), ans);
+        }
+        return ans;
+    }
+};
